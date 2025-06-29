6f7b35e3ab11281ad227c84eab1cbde846859f1f
rick <roman20071999@gmail.com>
2025-06-30 00:19:37 +0500

    hw_1

---
+++ ans_hw1/infra/locals.tf
@@ -0,0 +1,16 @@
+locals {
+  default_user = {
+    "ubuntu-2004-lts" = "ubuntu"
+    "centos-7"        = "centos"
+  }
+
+  hosts = [
+    for fam, inst in yandex_compute_instance.vm :
+    {
+      name = inst.name
+      ip   = inst.network_interface[0].nat_ip_address
+      user = lookup(local.default_user, fam, "ubuntu")
+      grp  = "webservers"
+    }
+  ]
+}
